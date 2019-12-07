32bb491539a509e82771849b0b44f88894418d6d
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-12-07 20:40:47 +0100

    fixtures etape 1 OK

--- src/DataFixtures/BienFixtures.php
+++ src/DataFixtures/BienFixtures.php
@@ -2,9 +2,7 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Bien;
-use App\Entity\Tipe;
-use App\Entity\Categorie;
+use App\Entity;
 use Doctrine\DBAL\Types\IntegerType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 //use Doctrine\ORM\EntityManagerInterface;
@@ -18,72 +16,90 @@ class BienFixtures extends Fixture
         // $manager->persist($product);
 
         $faker = \Faker\Factory::create();
+    
+        //Categorie
+        $datas = [
+            '1' => 'Appartement',
+            '2' => 'terrain',
+            '3' => 'Maison',
+        ];
+        foreach ($datas as $data) {
+            $item = new Entity\Categorie();
+            $item->setLibelle($data);
+            $manager->persist($item);
+        }
+        $manager->flush();
 
-                $libelle = "";
-                for($i = 1; $i <= 2; $i++) {
-                    $tipe = new Tipe();
-                    if($i == 1) {
-                        $libelle = "location";
-                    }
-                    else if($i == 2) {
-                        $libelle = "vente";
-                    }
-                
-                        $tipe->setLibelle($libelle);
-                    
-                        $manager->persist($tipe);
+        $categorieRepo = $manager->getRepository('App:Categorie');
+
+        //Types
+        $datas2 = [
+            '1' => 'location',
+            '2' => 'ventes',
+        ];
+
+        foreach ($datas2 as $data) {
+            $item = new Entity\Tipe();
+            $item->setLibelle($data);
+            $manager->persist($item);
+        }
+
+        $manager->flush();
+
+        $tipeRepo = $manager->getRepository('App:Tipe');
+
+        //Biens
+            for($k=1; $k <= 10; $k++) {
+                $bien = new Entity\Bien();
+                if($datas2 == 2) {
+                    $etage = 0;
+                    $chambre = 0;
+                    $surface = mt_rand(100,1000);
                 }
-            
-                $libelle2 = "";
-                for($j = 1; $j <= 4; $j++) {
-                    $categorie=new Categorie();
-                    if($j == 1) {
-                        $libelle2 = "appartement";
-                    }
-                    else if($j == 2) {
-                        $libelle2 = "terrain";
-                    }
-                    else if($j == 3) {
-                        $libelle2 = "pavillon";
-                    }
-                    else if($j == 4) {
-                        $libelle2 = "hml";
-                    }
-                
-                    $categorie->setLibelle($libelle2);
-
-                    $manager->persist($categorie);
-                    for($k=1; $k <= 10; $k++) {
-                        $bien = new Bien();
-                        if($j == 2) {
-                            $etage = 0;
-                            $chambre = 0;
-                        }
-                        else {
-                            $etage=mt_rand(1,10);
-                            $chambre=mt_rand(1,10);
-                        }
-                        if($j > 4 || $j < 6) {
-                            $statut = "archivé";
-                        }
-                        else {
-                            $statut = "en cours";
-                        }
-                        $bien->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true))
-                            ->setSurface(mt_rand(10,100))
-                            ->setEtage($etage)
-                            ->setChambre($chambre)
-                            ->setImage($faker->imageUrl($width = 300, $height = 200))
-                            ->setStatut($statut)
-                            ->setCreatedAt($faker->dateTimeBetween($startDate = '-3days', $endDate = 'now', $timezone = null))
-                            ->setCategorie($categorie)
-                            ->setTipe($tipe);
-
-                        $manager->persist($bien);
-                    }
+                else {
+                    $etage=mt_rand(1,10);
+                    $chambre=mt_rand(1,10);
+                    $surface = mt_rand(10,100);
                 }
-        
-        
+                if($k > 4 && $k < 6) {
+                    $statut = "archivé";
+                }
+                else {
+                    $statut = "en cours";
+                }
+                $bien->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true))
+                    ->setSurface($surface)
+                    ->setEtage($etage)
+                    ->setChambre($chambre)
+                    ->setImage($faker->imageUrl($width = 300, $height = 200))
+                    ->setStatut($statut)
+                    ->setCreatedAt($faker->dateTimeBetween($startDate = '-3days', $endDate = 'now', $timezone = null))
+                    ->setCategorie($categorieRepo->find(mt_rand(1, 2)))
+                    ->setTipe($tipeRepo->find(mt_rand(1, 2)));
+
+                $manager->persist($bien);
+            }
+                
+ 
         $manager->flush();
     }
 }
+
+        // Reset datas
+
+        // Supprimer la base de donnée
+        // php bin/console doctrine:database:drop --force
+        // php bin/console d:d:d --force
+
+        // Créer la basez de donnée
+        // php bin/console doctrine:database:create
+        // php bin/console d:d:c
+
+        // Regarder les requête SQL
+        // php bin/console doctrine:schema:updfate --dump-sql
+
+        // Mettre à jour la base de donnée
+        // php bin/console doctrine:schema:update --force
+        // php bin/console d:s:u -f
+
+        // Categories
