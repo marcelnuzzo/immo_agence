d7336818fdf39f757b15b8c7ada59f46f4d98c17
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-12-09 13:29:35 +0100

    clone done

--- src/Migrations/Version20191209122659.php
+++ src/Migrations/Version20191209122659.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191208051321 extends AbstractMigration
+final class Version20191209122659 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,7 +24,9 @@ final class Version20191208051321 extends AbstractMigration
 
         $this->addSql('CREATE TABLE bien (id INT AUTO_INCREMENT NOT NULL, tipe_id INT DEFAULT NULL, categorie_id INT DEFAULT NULL, description LONGTEXT NOT NULL, surface INT NOT NULL, etage INT NOT NULL, chambre INT NOT NULL, image VARCHAR(255) NOT NULL, statut VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_45EDC386C69A8E08 (tipe_id), INDEX IDX_45EDC386BCF5E72D (categorie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, phone INT NOT NULL, email VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, property VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tipe (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE bien ADD CONSTRAINT FK_45EDC386C69A8E08 FOREIGN KEY (tipe_id) REFERENCES tipe (id)');
         $this->addSql('ALTER TABLE bien ADD CONSTRAINT FK_45EDC386BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
     }
@@ -38,6 +40,8 @@ final class Version20191208051321 extends AbstractMigration
         $this->addSql('ALTER TABLE bien DROP FOREIGN KEY FK_45EDC386C69A8E08');
         $this->addSql('DROP TABLE bien');
         $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE contact');
         $this->addSql('DROP TABLE tipe');
+        $this->addSql('DROP TABLE user');
     }
 }
