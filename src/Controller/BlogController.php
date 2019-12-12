0bcbab4ac7c9f61e364797e5fcdef3f11b09b8c9
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-12-12 13:16:34 +0100

    cat + tip OK

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Bien;
-use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Tipe;
+use App\Entity\Categorie;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,7 +18,35 @@ class BlogController extends AbstractController
     public function index()
     {
         return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController'
+        ]);
+    }
+
+    /**
+     * @Route("/cat", name="cat")
+     */
+    public function cat()
+    {
+        $repo = $this->getDoctrine()->getRepository(Categorie::class);
+        $categories = $repo->findAll();
+
+        return $this->render('blog/cat.html.twig', [
             'controller_name' => 'BlogController',
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/tip", name="tip")
+     */
+    public function tip()
+    {
+        $repo = $this->getDoctrine()->getRepository(Tipe::class);
+        $tipes = $repo->findAll();
+
+        return $this->render('blog/tip.html.twig', [
+            'controller_name' => 'BlogController',
+            'tipes' => $tipes
         ]);
     }
 
